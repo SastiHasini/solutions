2bd17af1c04765bd40b394b886f00acab7e6b470
SastiHasini <sastihasini@gmail.com>
2026-07-09 20:40:13 +0530

    Added BookStoreManagement

---
+++ BookStoreManagement/src/main/java/com/bookstore/BookStoreApplication.java
@@ -0,0 +1,8 @@
+package com.bookstore;
+
+public class BookStoreApplication {
+
+    public static void main(String[] args) {
+        System.out.println("BookStore Management Application Started");
+    }
+}
